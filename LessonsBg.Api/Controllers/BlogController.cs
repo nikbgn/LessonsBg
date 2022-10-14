3ff5bbe835dafd45a67cd4c68903e214127bba9b
Nikolai <pythondevbg8@abv.bg>
2022-10-14 19:59:27 +0300

    Api GetAll implemented

---
+++ LessonsBg.Api/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿namespace LessonsBg.Api.Controllers
+{
+    using LessonsBg.Core.Contracts;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogService blogService;
+
+        public BlogController(IBlogService _blogService)
+        {
+            blogService = _blogService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await blogService.GetAll());
+        }
+    }
+}
