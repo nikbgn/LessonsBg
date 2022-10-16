3faf3c8910ee1da41963871b4a716f62c980fedd
Nikolai <pythondevbg8@abv.bg>
2022-10-16 16:12:47 +0300

    API and Documentation update

--- LessonsBg.Api/Controllers/BlogController.cs
+++ LessonsBg.Api/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿namespace LessonsBg.Api.Controllers
 {
     using LessonsBg.Core.Contracts;
+    using LessonsBg.Core.Models;
 
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
@@ -11,12 +12,24 @@
     {
         private readonly IBlogService blogService;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_blogService"></param>
+
         public BlogController(IBlogService _blogService)
         {
             blogService = _blogService;
         }
 
+        /// <summary>
+        /// Get all blog posts.
+        /// </summary>
+        /// <returns></returns>
+        /// 
         [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(IEnumerable<BlogPostDto>))]
         public async Task<IActionResult> GetAll()
         {
             return Ok(await blogService.GetAll());
--- LessonsBg.Api/Program.cs
+++ LessonsBg.Api/Program.cs
@@ -1,6 +1,8 @@
 using LessonsBg.Core.Contracts;
 using LessonsBg.Core.Services;
 
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1" , new OpenApiInfo()
+    {
+        Title = "Lessons BG",
+        Description = "The best platform for finding tutors."
+    });
+    c.IncludeXmlComments("LessonsBgApiDocumentation.xml");
+});
 
 builder.Services.AddScoped<IBlogService, BlogService>();
 
